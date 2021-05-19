25b34446164fe74304f9226e9520b91972b20828
Eva Chitul <eva.chitul@coleurope.eu>
2021-05-19 09:21:35 +0200

    Updates Profile Info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { RegisterUserComponent } from './user/register-user/register-user.component';
 import { UserProfileComponent } from './user/user-profile/user-profile.component';
+import {LoginFormComponent} from "./user/login-form/login-form.component";
 
 const routes: Routes = [
   {path: '', component : HomePageComponent },
   {path: 'register', component: RegisterUserComponent},
-  {path: 'users/:id', component: UserProfileComponent}
+  {path: 'users/:id', component: UserProfileComponent},
+  {path: 'login', component: LoginFormComponent}
 ];
 
 @NgModule({
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { User } from '../modules/user';
 import { environment } from 'src/environments/environment';
+import {HttpHeaders} from "@angular/common/http";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +26,18 @@ export class UserService {
     return this._http.get<User>(`${this._userUrl}/${id}`);
   }
 
+  loginUser(credentials): Observable<User> {
 
+    const httpOptions = {
+      headers: new HttpHeaders({
+        email: credentials.email,
+        password: credentials.password
+      })
+    };
+
+    return this._http.get<User>(`${this._userUrl}/login`, httpOptions);
+
+
+  }
 
 }
