54904dba0f4f1a6f85abbb98706d969cfede8b33
Eva Chitul <eva.chitul@coleurope.eu>
2021-05-28 10:42:59 +0200

    Minor refactoring frontend

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -8,15 +8,26 @@ import { AuthService } from 'src/app/auth/auth.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
   private _isLoggedIn: boolean;
+  private _userId: string;
 
   constructor(private _authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this._isLoggedIn = this._authService.isAuthenticated();
+    this._userId = this.getUserId();
   }
 
   //TODO: Switch sign in button to sign out when signed in
+
+  get userId(): string {
+    return this._userId;
+  }
+
+  getUserId(): string {
+    return this._authService.getCurrentUserId();
+  }
 
   get isLoggedIn() {
     return this._isLoggedIn;
