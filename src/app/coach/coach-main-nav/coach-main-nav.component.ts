8df9a529067ca923fee8f8abec8c57a1277287e4
Gertjan Goetynck <gertjan@goetynck.com>
2021-06-01 16:43:46 +0200

    Added polishes

--- src/app/coach/coach-main-nav/coach-main-nav.component.ts
+++ src/app/coach/coach-main-nav/coach-main-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-coach-main-nav',
@@ -8,17 +9,18 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CoachMainNavComponent implements OnInit {
   private _userId: string;
 
-  constructor() { }
+  constructor(private _authService: AuthService) { }
 
   ngOnInit(): void {
+
   }
 
   get userId(): string {
     return this._userId;
   }
 
-  @Input() set userId(userId: string) {
-    this._userId = userId;
+  getUserId(): string {
+    return this._authService.getCurrentUserId();
   }
 
 }
